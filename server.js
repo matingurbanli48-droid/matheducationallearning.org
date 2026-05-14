04a5879c9806ab581668625fe55f5dd40ea780a7
matingurbanli48-droid <matingurbanli48@gmail.com>
2026-05-13 21:20:32 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,15 @@
-import express from "express";
-import { createServer } from "http";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
-import { server as wisp } from "@mercuryworkshop/wisp-js/server";
+const express = require("express");
+const { createServer } = require("http");
+const path = require("path");
+const wisp = require("wisp-server-node");
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use(express.static(join(__dirname, "public")));
-
-// Log what's in the scramjet folder so we can see the right path
-import { readdirSync } from "fs";
-try {
-  const scramjetFiles = readdirSync(join(__dirname, "node_modules/@mercuryworkshop/scramjet"));
-  console.log("Scramjet folder contents:", scramjetFiles);
-} catch(e) {
-  console.log("Scramjet folder error:", e.message);
-}
-
-app.use("/scram/", express.static(join(__dirname, "node_modules/@mercuryworkshop/scramjet/dist")));
-app.use("/baremux/", express.static(join(__dirname, "node_modules/@mercuryworkshop/bare-mux/dist")));
-app.use("/epoxy/", express.static(join(__dirname, "node_modules/@mercuryworkshop/epoxy-transport/dist")));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uv/", express.static(path.join(__dirname, "node_modules/@titaniumnetwork-dev/ultraviolet/dist")));
 
 const server = createServer(app);
+
 server.on("upgrade", (req, socket, head) => {
   wisp.routeRequest(req, socket, head);
 });
