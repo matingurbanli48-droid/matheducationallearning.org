953096eb3e7a7092d150be05a4c4c4deeb84ad3d
matingurbanli48-droid <matingurbanli48@gmail.com>
2026-05-13 20:28:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-const express = require("express");
-const { createBareServer } = require("bare-server-node");
-const path = require("path");
-const http = require("http");
+import express from "express";
+import { createServer } from "http";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+import { scramjetPath } from "@mercuryworkshop/scramjet/path";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/path";
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport/path";
+import { WispServer } from "@mercuryworkshop/wisp-js/server";
 
-const bare = createBareServer("/bare/");
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use("/scramjet/", express.static(path.join(__dirname, "node_modules/@mercuryworkshop/scramjet/dist")));
+app.use(express.static(join(__dirname, "public")));
+app.use("/scram/", express.static(scramjetPath));
+app.use("/baremux/", express.static(baremuxPath));
+app.use("/epoxy/", express.static(epoxyPath));
 
-const server = http.createServer((req, res) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeRequest(req, res);
-  } else {
-    app(req, res);
-  }
-});
-
-server.on("upgrade", (req, socket, head) => {
-  if (bare.shouldRoute(req)) {
-    bare.routeUpgrade(req, socket, head);
-  } else {
-    socket.destroy();
-  }
-});
+const server = createServer(app);
+const wisp = new WispServer({ server });
+wisp.start("/wisp/");
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Running on port ${PORT}`));
