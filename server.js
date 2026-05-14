eed81278afe24aa423de6af626557a6bd1452a6f
matingurbanli48-droid <matingurbanli48@gmail.com>
2026-05-13 20:31:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,15 @@ import express from "express";
 import { createServer } from "http";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
-import { scramjetPath } from "@mercuryworkshop/scramjet/path";
-import { baremuxPath } from "@mercuryworkshop/bare-mux/path";
-import { epoxyPath } from "@mercuryworkshop/epoxy-transport/path";
 import { WispServer } from "@mercuryworkshop/wisp-js/server";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
 app.use(express.static(join(__dirname, "public")));
-app.use("/scram/", express.static(scramjetPath));
-app.use("/baremux/", express.static(baremuxPath));
-app.use("/epoxy/", express.static(epoxyPath));
+app.use("/scram/", express.static(join(__dirname, "node_modules/@mercuryworkshop/scramjet/dist")));
+app.use("/baremux/", express.static(join(__dirname, "node_modules/@mercuryworkshop/bare-mux/dist")));
+app.use("/epoxy/", express.static(join(__dirname, "node_modules/@mercuryworkshop/epoxy-transport/dist")));
 
 const server = createServer(app);
 const wisp = new WispServer({ server });
