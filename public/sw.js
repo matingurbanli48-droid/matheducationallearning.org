69da6efed61ddafcf2c206379746591d32726529
matingurbanli48-droid <matingurbanli48@gmail.com>
2026-05-13 21:19:42 -0400

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-importScripts("https://cdn.jsdelivr.net/npm/@mercuryworkshop/scramjet@0.0.9/dist/scramjet.all.js");
-const { ScramjetServiceWorker } = $scramjetLoadWorker();
-const sw = new ScramjetServiceWorker();
-self.addEventListener("fetch", (event) => {
-  event.respondWith((async () => {
-    await sw.loadConfig();
-    if (sw.route(event)) return sw.fetch(event);
-    return fetch(event.request);
-  })());
+importScripts("/uv/uv.bundle.js");
+importScripts("/uv/uv.config.js");
+
+const sw = new UVServiceWorker();
+
+self.addEventListener("fetch", event => {
+  if (sw.route(event)) {
+    event.respondWith(sw.fetch(event));
+  }
 });
